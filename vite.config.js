6ae96a4430f8635c0bb7a18e81ccb1abd100cace
Maicon Jeison <maiconjeison24@gmail.com>
2024-10-25 16:02:17 -0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import copy from 'rollup-plugin-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    copy({
+      targets: [
+        { src: 'src/assets/*', dest: 'dist/assets' }
+      ]
+    })
+  ],
+  server: {
+    port: 3000,
+    open: true,
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+  },
+});
